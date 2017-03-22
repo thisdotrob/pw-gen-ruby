649d185d4271967933a7b8eb039115710c227bd2
thisdotrob <robstevenson000@gmail.com>
2017-03-22 21:59:20 +0000

    Test length of returned password

--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -1,10 +1,14 @@
 require 'password_generator'
 
-RSpec.describe generate_password do
-  describe '#generate_password' do
-    it 'returns a string' do
-      password = generate_password
-      expect(password).to be_a(String)
-    end
+describe '#generate_password' do
+  it 'returns a string' do
+    password = generate_password(11)
+    expect(password).to be_a(String)
+  end
+
+  it 'returns a password of the specified length' do
+    test_length = 33
+    password = generate_password(test_length)
+    expect(password.length).to eq(test_length)
   end
 end
