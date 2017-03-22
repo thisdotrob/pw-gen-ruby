8462ce58b4d609aed091711e36f0dfa695c45d43
thisdotrob <robstevenson000@gmail.com>
2017-03-22 22:21:15 +0000

    Test that uppercase characters are used

--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -2,13 +2,20 @@ require 'password_generator'
 
 describe '#generate_password' do
   it 'returns a string' do
-    password = generate_password(11)
+    password = generate_password(11, true)
     expect(password).to be_a(String)
   end
 
   it 'returns a password of the specified length' do
     test_length = 33
-    password = generate_password(test_length)
+    password = generate_password(test_length, true)
     expect(password.length).to eq(test_length)
+  end
+
+  context 'when \'uppercase\' is true' do
+    it 'returns a password of uppercase characters' do
+      password = generate_password(15, true)
+      expect(password).to match(/^[A-Z]+$/)
+    end
   end
 end
