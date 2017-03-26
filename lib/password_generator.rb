d7a0f2c3e2456704c9411ec3595f8abf76922a6e
thisdotrob <robstevenson000@gmail.com>
2017-03-26 23:57:28 +0100

    Use alternating character sets

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -1,12 +1,24 @@
 def generate_password(length, uppercase:, lowercase:, number:, special:)
-  char = if uppercase
-           'A'
-         elsif lowercase
-           'a'
-         elsif number
-           '0'
-         else
-           '!'
-         end
-  char * length
+  queue = initialise_character_sets(uppercase, lowercase, number, special)
+
+  password = ''
+
+  while password.length < length
+    char = queue.shift
+    password += char
+    queue << char
+  end
+
+  password
+end
+
+def initialise_character_sets(uppercase, lowercase, number, special)
+  queue = []
+
+  queue << 'A' if uppercase
+  queue << 'a' if lowercase
+  queue << '0' if number
+  queue << '!' if special\
+
+  queue
 end
