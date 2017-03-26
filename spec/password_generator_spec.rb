8bb5559492ad944804e60343726c176c2010e328
thisdotrob <robstevenson000@gmail.com>
2017-03-26 09:01:13 +0100

    Test numbers characters are used if number is true

--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -2,27 +2,34 @@ require 'password_generator'
 
 describe '#generate_password' do
   it 'returns a string' do
-    password = generate_password(11, lowercase: true, uppercase: true)
+    password = generate_password(11, lowercase: true, uppercase: true, number: true)
     expect(password).to be_a(String)
   end
 
   it 'returns a password of the specified length' do
     test_length = 33
-    password = generate_password(test_length, lowercase: true, uppercase: true)
+    password = generate_password(test_length, lowercase: true, uppercase: true, number: false)
     expect(password.length).to eq(test_length)
   end
 
   context 'when \'uppercase\' is true' do
     it 'returns a password of only uppercase characters' do
-      password = generate_password(15, lowercase: false, uppercase: true)
+      password = generate_password(15, lowercase: false, uppercase: true, number: false)
       expect(password).to match(/^[A-Z]+$/)
     end
   end
 
   context 'when \'lowercase\' is true' do
     it 'returns a password of only lowercase characters' do
-      password = generate_password(15, uppercase: false, lowercase: true)
+      password = generate_password(15, uppercase: false, lowercase: true, number: false)
       expect(password).to match(/^[a-z]+$/)
+    end
+  end
+
+  context 'when \'number\' is true' do
+    it 'returns a password of only numeric characters' do
+      password = generate_password(15, uppercase: false, lowercase: false, number: true)
+      expect(password).to match(/^[0-9]+$/)
     end
   end
 end
