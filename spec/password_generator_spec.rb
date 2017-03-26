dc738ddb466a1f6bf7407410ebabe1c281af3f2a
thisdotrob <robstevenson000@gmail.com>
2017-03-26 09:07:10 +0100

    Test special characters are used if special is true

--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -2,34 +2,41 @@ require 'password_generator'
 
 describe '#generate_password' do
   it 'returns a string' do
-    password = generate_password(11, lowercase: true, uppercase: true, number: true)
+    password = generate_password(11, lowercase: true, uppercase: true, number: true, special: true)
     expect(password).to be_a(String)
   end
 
   it 'returns a password of the specified length' do
     test_length = 33
-    password = generate_password(test_length, lowercase: true, uppercase: true, number: false)
+    password = generate_password(test_length, lowercase: true, uppercase: true, number: false, special: false)
     expect(password.length).to eq(test_length)
   end
 
   context 'when \'uppercase\' is true' do
     it 'returns a password of only uppercase characters' do
-      password = generate_password(15, lowercase: false, uppercase: true, number: false)
+      password = generate_password(15, lowercase: false, uppercase: true, number: false, special: false)
       expect(password).to match(/^[A-Z]+$/)
     end
   end
 
   context 'when \'lowercase\' is true' do
     it 'returns a password of only lowercase characters' do
-      password = generate_password(15, uppercase: false, lowercase: true, number: false)
+      password = generate_password(15, uppercase: false, lowercase: true, number: false, special: false)
       expect(password).to match(/^[a-z]+$/)
     end
   end
 
   context 'when \'number\' is true' do
     it 'returns a password of only numeric characters' do
-      password = generate_password(15, uppercase: false, lowercase: false, number: true)
+      password = generate_password(15, uppercase: false, lowercase: false, number: true, special: false)
       expect(password).to match(/^[0-9]+$/)
+    end
+  end
+
+  context 'when \'special\' is true' do
+    it 'returns a password containing only special characters' do
+      password = generate_password(15, uppercase: false, lowercase: false, number: false, special: true)
+      expect(password).to match(/^[!$%&*@^]+$/)
     end
   end
 end
