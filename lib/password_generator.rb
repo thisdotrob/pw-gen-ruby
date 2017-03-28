72525ba75530c0d9146df80422ffcf1a33bf20cd
thisdotrob <robstevenson000@gmail.com>
2017-03-28 08:55:39 +0100

    Pick uppercase characters randomly

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -4,9 +4,11 @@ def generate_password(length, uppercase:, lowercase:, number:, special:)
   password = ''
 
   while password.length < length
-    char = queue.shift
+    charSet = queue.shift
+    index = rand(charSet.length)
+    char = charSet[index]
     password += char
-    queue << char
+    queue << charSet
   end
 
   password
@@ -15,10 +17,10 @@ end
 def initialise_character_sets(uppercase, lowercase, number, special)
   queue = []
 
-  queue << 'A' if uppercase
+  queue << 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if uppercase
   queue << 'a' if lowercase
   queue << '0' if number
-  queue << '!' if special\
+  queue << '!' if special
 
   queue
 end
