f1d3c9372e631aafd1aa34acfc17c076d0dda601
thisdotrob <robstevenson000@gmail.com>
2017-03-30 18:39:39 +0100

    Use single, concatenated character set

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -1,26 +1,24 @@
 def generate_password(length, uppercase:, lowercase:, number:, special:)
-  queue = initialise_character_sets(uppercase, lowercase, number, special)
+  charSet = initialise_character_sets(uppercase, lowercase, number, special)
 
   password = ''
 
   while password.length < length
-    charSet = queue.shift
     index = rand(charSet.length)
     char = charSet[index]
     password += char
-    queue << charSet
   end
 
   password
 end
 
 def initialise_character_sets(uppercase, lowercase, number, special)
-  queue = []
+  charSet = ''
 
-  queue << 'abcdefghijklmnopqrstuvwxyz' if lowercase
-  queue << 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if uppercase
-  queue << '0123456789' if number
-  queue << '!$%&*@^' if special
+  charSet << '0123456789' if number
+  charSet << 'abcdefghijklmnopqrstuvwxyz' if lowercase
+  charSet << 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if uppercase
+  charSet << '!$%&*@^' if special
 
-  queue
+  charSet
 end
